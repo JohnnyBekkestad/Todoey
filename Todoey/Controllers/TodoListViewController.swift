32ef0228f2e42433513bc98bc9e30931fe47ff03
Johnny Bekkestad <johnny.bekkestad@gmail.com>
2019-07-06 14:02:17 -0500

    Implementing CRUD

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,17 +7,18 @@
 //
 
 import UIKit
-
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
-//    let defaults = UserDefaults.standard
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadItems()
     }
     
@@ -32,17 +33,27 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = item.title
-        cell.accessoryType = item.selected ? .checkmark : .none
+        cell.accessoryType = item.isSelected ? .checkmark : .none
         return cell
     }
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        itemArray[indexPath.row].selected = !itemArray[indexPath.row].selected
+
         
+        itemArray[indexPath.row].isSelected = !itemArray[indexPath.row].isSelected
         saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            context.delete(itemArray[indexPath.row])
+            self.itemArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            saveItems()
+        }
     }
     
     //MARK - Add new Items
@@ -54,7 +65,10 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What will happend once the User clicks the Add Item Button on our UIAlert
-            let item : Item = Item(name: localTextField.text!)
+            
+            let item = Item(context: self.context)
+            item.title = localTextField.text
+            item.isSelected = false
             self.itemArray.append(item)
             self.saveItems()
         }
@@ -71,26 +85,22 @@ class TodoListViewController: UITableViewController {
     //MARK: Model Manipulation methods
 
     func saveItems(){
-        let encoder = PropertyListEncoder()
         do
         {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         }catch{
-            print("Error encoding itemArray, \(error)")
+            print("Error saving data to context, \(error)")
         }
         tableView.reloadData()
     }
     
     func loadItems(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do
-            {
-                itemArray = try decoder.decode([Item].self, from: data)
-            }catch {
-                print("Error decoding itemArray, \(error)")
-            }
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do
+        {
+            itemArray = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context, \(error)")
         }
     }
 }
